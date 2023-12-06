a41ac0ee8e7cc4432ea2a55fdcc2257539a03d16
Guilherme Neves da Silva <guilhermenevesdasilva321@gmail.com>
2023-12-06 20:39:58 -0300

    doc(_layout): documentation of _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,123 +1,28 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { SplashScreen, Stack } from "expo-router";
-import { useEffect } from "react";
-import { View, useColorScheme } from "react-native";
-import { SessionProvider } from "../context/AuthContext";
-import { RootSiblingParent } from "react-native-root-siblings";
-import Background from "../components/Background";
-import { theme } from "../core/theme";
+import { Text, useColorScheme } from "react-native";
+import { useSession } from "../../context/AuthContext";
+import { Redirect, Tabs } from "expo-router";
+import Login from "./login";
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from "expo-router";
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(auth-routes)",
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
+export default function AuthLayout() {
+  const sessionInfo = useSession();
   const colorScheme = useColorScheme();
 
-  return (
-    <RootSiblingParent>
-      <SessionProvider>
-        <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-          <Stack>
-            <Stack.Screen
-              name="(user-routes)"
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="(auth-routes)"
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="new_device_setting"
-              options={{
-                presentation: "card",
-                headerTitle: "Criar Pré-definição",
-                headerStyle: { backgroundColor: theme.colors.primary },
-                headerTintColor: "#fff",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-            <Stack.Screen
-              name="new_device"
-              options={{
-                presentation: "card",
-                headerTitle: "Criar Dispositivo",
-                headerStyle: { backgroundColor: theme.colors.primary },
-                headerTintColor: "#fff",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-            <Stack.Screen
-              name="edit_device"
-              options={{
-                presentation: "card",
-                headerTitle: "Editar dispositivo",
-                headerStyle: { backgroundColor: theme.colors.primary },
-                headerTintColor: "#fff",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-            <Stack.Screen
-              name="edit_device_setting"
-              options={{
-                presentation: "card",
-                headerTitle: "Editar Pré-definição",
-                headerStyle: { backgroundColor: theme.colors.primary },
-                headerTintColor: "#fff",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-          </Stack>
-        </ThemeProvider>
-      </SessionProvider>
-    </RootSiblingParent>
-  );
+  if (!sessionInfo) {
+    // Lidar com o caso em que sessionInfo é null
+    return <Text>Session info not available</Text>;
+  }
+
+  const { session, isLoading } = sessionInfo;
+
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (session) {
+    // On web, static rendering will stop here as the user is not authenticated
+    // in the headless Node process that the pages are rendered in.
+    return <Redirect href="/(user-routes)" />;
+  }
+
+  return <Login />;
 }
